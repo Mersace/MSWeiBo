c7f5d577d3a6df2f3a724cac7ea729d8992ba0b5
Mersace <ma815305712@outlook.com>
2017-05-21 15:42:04 +0800

    Initial Commit

---
+++ MSWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSWeiBo
+//
+//  Created by Ashscar on 17/5/21.
+//  Copyright © 2017年 Ashscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
